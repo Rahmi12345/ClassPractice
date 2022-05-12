5799beeb7f13a79cf41cc68cb309c2e093b1e944
Asiel Safi <rahmisafi@Rahmis-iMac.lan>
2022-05-11 22:36:29 -0700

    new

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -2,7 +2,7 @@
 public class GitHubClass {
 	public static void main(String[] args) {
 		int a = 12;
-		int b= 1;
+		int b= 12;
 		System.out.println(a+b);
 		// this changes are from remote repo.
 	}
--- src/hhh.java
+++
@@ -1,8 +0,0 @@
-
-public class hhh {
-
-	public static void main(String[] args) {
-		// this is me...
-	}
-
-}
